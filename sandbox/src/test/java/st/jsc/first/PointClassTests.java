2490218ec98b633635386fbfa3c9b8673cb84467
Alexander Strateichuk <a.strateichuk@gmail.com>
2016-05-28 20:43:31 +0300

    Point Class Unit tests completed

--- sandbox/src/test/java/st/jsc/first/PointClassTests.java
+++ sandbox/src/test/java/st/jsc/first/PointClassTests.java
@@ -1,8 +1,10 @@
 package st.jsc.first;
 
+import org.apache.commons.math3.util.Precision;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.apache.commons.math3.util.Precision;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 
 /**
@@ -16,8 +18,9 @@ public class PointClassTests {
 
         Point p1 = new Point(-2.3,8);
         Point p2 = new Point(8.5,0.7);
-        Assert.assertEquals(Precision.round(p1.distanceToPoint(p2),3), 13.036);
-        Assert.assertEquals(Precision.round(p2.distanceToPoint(p1),3), 13.036);
+        double expectedDistance = 13.036;
+        Assert.assertEquals(Precision.round(p1.distanceToPoint(p2),3), expectedDistance);
+        Assert.assertEquals(Precision.round(p2.distanceToPoint(p1),3), expectedDistance);
 
     }
 
@@ -25,20 +28,33 @@ public class PointClassTests {
     public void DistanceCheckPrecise()
     {
 
-        Point p1 = new Point(-2.3,8);
-        Point p2 = new Point(8.5,0.7);
-        Assert.assertEquals(Precision.round(p1.distanceToPoint(p2),3), 13.036);
-        Assert.assertEquals(Precision.round(p2.distanceToPoint(p1),3), 13.036);
+        Point p1 = new Point(-10,100);
+        Point p2 = new Point(2.5,0.75);
+        double expectedDistance = 100.03405670070569;
+        Assert.assertEquals(p1.distanceToPoint(p2), expectedDistance);
+        Assert.assertEquals(p2.distanceToPoint(p1), expectedDistance);
 
     }
     @Test
     public void DistanceCheckZeroPoint()
     {
 
-        Point p1 = new Point(-2.3,8);
-        Point p2 = new Point(8.5,0.7);
-        Assert.assertEquals(Precision.round(p1.distanceToPoint(p2),3), 13.036);
-        Assert.assertEquals(Precision.round(p2.distanceToPoint(p1),3), 13.036);
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(0.5,0.7);
+        double expectedDistance = 0.8602325267042626;
+        Assert.assertEquals(p1.distanceToPoint(p2), expectedDistance);
+        Assert.assertEquals(p2.distanceToPoint(p1), expectedDistance);
+
+    }
+
+    @Test
+    public void DistanceCheckRandomThruFunction()
+    {
+        Point p1 = new Point(ThreadLocalRandom.current().nextDouble(-100,100),ThreadLocalRandom.current().nextDouble(-100,100));
+        Point p2 = new Point(ThreadLocalRandom.current().nextDouble(-100,100),ThreadLocalRandom.current().nextDouble(-100,100));
+        double expectedDistance = MyFirstProgram.distance(p1,p2);
+        Assert.assertEquals(p1.distanceToPoint(p2), expectedDistance);
+        Assert.assertEquals(p2.distanceToPoint(p1), expectedDistance);
 
     }
 
