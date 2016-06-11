3aeef941750a1145528db76fc3e73fc4680dbb35
Alexander Strateichuk <a.strateichuk@gmail.com>
2016-06-11 23:42:45 +0300

    Groups lists sorting is added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -29,16 +29,13 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
 
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
+        return name != null ? name.hashCode() : 0;
     }
 
     public GroupData(int id, String name, String header, String footer) {
@@ -48,10 +45,11 @@ public class GroupData {
         this.header = header;
         this.footer = footer;
 
+
     }
 
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
