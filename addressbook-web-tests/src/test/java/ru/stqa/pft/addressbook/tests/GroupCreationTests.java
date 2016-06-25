6d2b2cb08e2f051afa10ff4b19f752c23c5ac9ae
Alexander Strateichuk <a.strateichuk@gmail.com>
2016-06-25 19:24:35 +0300

    Contacts read from data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,14 +16,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
 
-   @DataProvider
-   public Iterator<Object[]> validGroups(){
-       List<Object[]> list = new ArrayList<Object[]>();
-       list.add(new Object[] {new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-       list.add(new Object[] {new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-       list.add(new Object[] {new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
-       return list.iterator();
-   }
+    @DataProvider
+    public Iterator<Object[]> validGroups() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null && !line.equals("")) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
+        return list.iterator();
+    }
 
     @Test(dataProvider = "validGroups")
     public void testGroupCreation(GroupData group) {
@@ -32,7 +37,7 @@ public class GroupCreationTests extends TestBase {
         app.group().create(group);
         Groups after = app.group().getAll();
         assertThat(after.size(), equalTo(before.size() + 1));
-        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
 
     }
 
@@ -45,7 +50,7 @@ public class GroupCreationTests extends TestBase {
         app.group().create(group);
         assertThat(app.group().getGroupCount(), equalTo(before.size()));
         Groups after = app.group().getAll();
-               assertThat(after, equalTo(before));
+        assertThat(after, equalTo(before));
 
     }
 
